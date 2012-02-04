b408683717240b9249c5d9a55ef8693f95d76a8f
Nicholas J Humfrey <njh@aelius.com>
2012-02-04 14:18:10 +0000

    Restored mqtt_version_spec.rb and changed it to check EventMachine::MQTT::VERSION instead

---
+++ spec/em_mqtt_version_spec.rb
@@ -0,0 +1,23 @@
+$:.unshift(File.dirname(__FILE__))
+
+require 'spec_helper'
+require 'em-mqtt'
+
+describe EventMachine::MQTT do
+
+  describe "version number" do
+    it "should be defined as a constant" do
+      defined?(EventMachine::MQTT::VERSION).should == 'constant'
+    end
+
+    it "should be a string" do
+      EventMachine::MQTT::VERSION.should be_a(String)
+    end
+ 
+    it "should be in the format x.y.z" do
+      EventMachine::MQTT::VERSION.should =~ /^\d{1,2}\.\d{1,2}\.\d{1,2}$/
+    end
+  
+  end   
+
+end
