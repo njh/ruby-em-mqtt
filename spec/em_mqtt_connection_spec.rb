77ebb96dab06909b404c913a450f4e5b3045904d
Nicholas Humfrey <njh@aelius.com>
2015-03-01 11:41:16 +0000

    Changed to rspec 3 expect().to syntax

--- spec/em_mqtt_connection_spec.rb
+++ spec/em_mqtt_connection_spec.rb
@@ -4,17 +4,18 @@ require 'spec_helper'
 
 describe EventMachine::MQTT::Connection do
 
-  let(:subject) { EventMachine::MQTT::Connection.new(mock) }
+  let(:signature) { double('signature') }
+  let(:subject) { EventMachine::MQTT::Connection.new(signature) }
   let(:packet) { MQTT::Packet::Publish.new( :topic => 'test', :payload => 'hello world' ) }
 
   describe "when receiving data" do
     it "should parse packets" do
-      subject.should_receive(:process_packet).exactly(:once)
+      expect(subject).to receive(:process_packet).exactly(:once)
       subject.receive_data packet.to_s
     end
 
     it "should handle multiple packets at once" do
-      subject.should_receive(:process_packet).exactly(:twice)
+      expect(subject).to receive(:process_packet).exactly(:twice)
       subject.receive_data [packet, packet].join
     end
   end
--- spec/em_mqtt_version_spec.rb
+++ spec/em_mqtt_version_spec.rb
@@ -6,17 +6,16 @@ describe EventMachine::MQTT do
 
   describe "version number" do
     it "should be defined as a constant" do
-      defined?(EventMachine::MQTT::VERSION).should == 'constant'
+      expect(defined?(EventMachine::MQTT::VERSION)).to eq('constant')
     end
 
     it "should be a string" do
-      EventMachine::MQTT::VERSION.should be_a(String)
+      expect(EventMachine::MQTT::VERSION).to be_a(String)
     end
  
     it "should be in the format x.y.z" do
-      EventMachine::MQTT::VERSION.should =~ /^\d{1,2}\.\d{1,2}\.\d{1,2}$/
+      expect(EventMachine::MQTT::VERSION).to match(/^\d{1,2}\.\d{1,2}\.\d{1,2}$/)
     end
-  
   end   
 
 end
